dff109d71060d27fe105729c51fe0eee6c6e5060
Paulo Souza <jr.paulo87@gmail.com>
2022-08-14 22:13:34 -0300

    feat: update the counter of tasks amount and done tasks amount

--- src/components/TasksHeader/index.tsx
+++ src/components/TasksHeader/index.tsx
@@ -1,23 +1,36 @@
+import { useContext } from "react";
 import { Text, View } from "react-native";
+import { Context } from "../../shared/context/Context";
 import { styles } from "./styles";
 
 export function TasksHeader(){
+  const { tasks } = useContext(Context);
+  const tasksAmount = tasks.length;
+  const doneTasksAmount = tasks
+    .filter(task => task.isDone === true).length;
+
   return (
     <View style={styles.tasksHeader}>
       <View style={styles.amountContainer}>
         <Text style={styles.created}>
-          Criadas
+          { tasksAmount === 1
+            ? 'Criada'
+            : 'Criadas'
+          }
         </Text>
         <Text style={styles.taskAmount}>
-          0
+          { tasksAmount }
         </Text>
       </View>
       <View style={styles.amountContainer}>
         <Text style={styles.completed}>
-          Concluídas
+          { doneTasksAmount === 1
+            ? 'Concluída'
+            : 'Concluídas'
+          }
         </Text>
         <Text style={styles.taskAmount}>
-          0
+          { doneTasksAmount }
         </Text>
       </View>
     </View>
